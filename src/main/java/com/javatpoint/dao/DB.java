403e994085ca7dc3fe3a3351e133eaf997d9d9fc
Saurabh Gambhire <saurabhgambhire57@gmail.com>
2023-04-16 12:15:22 +0530

    fee-management-system

---
+++ src/main/java/com/javatpoint/dao/DB.java
@@ -0,0 +1,22 @@
+package com.javatpoint.dao;
+import java.sql.*;
+public class DB 
+	{
+		static Connection con;
+	
+	public static Connection getCon() throws ClassNotFoundException, SQLException 
+		
+	{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String url = "jdbc:mysql://localhost:3306/project";
+		String user = "root";
+		String password = "saurah";
+				
+			con  =	DriverManager.getConnection(url,user,password);
+		
+		
+		return con;		
+	}
+
+}
